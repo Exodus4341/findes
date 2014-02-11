e4852b7eb58fe9b5113f71610431f1d86d222640
Exodus4341 <genz.ortiz@gmail.com>
2014-02-10 19:42:43 -0800

    gitignore added

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package     Fuel
+ * @version     1.6
+ * @author      Fuel Development Team
+ * @license     MIT License
+ * @copyright   2010 - 2013 Fuel Development Team
+ * @link        http://fuelphp.com
+ */
+
+return array (
+	'crypto_key' => 'wV8HWkVxwvHcXq4edcniwdIY',
+	'crypto_iv' => 'lvIM28i7YBso07kvOQmtQsz0',
+	'crypto_hmac' => 'yQQHCwC1EZuAcCIboMOXgW04',
+);
