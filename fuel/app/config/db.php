a23c8caf58ece26bac0061a43b5da8dd7358a6c8
Exodus4341 <genz.ortiz@gmail.com>
2014-02-11 09:46:23 -0800

    db config

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -8,7 +8,7 @@
 return array (
      'Default' => array (
          'Connection' => array (
-             . 'Dsn' =>.... 'Mysql: host =' $ _SERVER ['DB1_HOST'] '; port =' $ _SERVER ['DB1_PORT'] '; dbname =' $ _SERVER ['DB1_NAME'],
+             'Dsn' =>.... 'Mysql: host =' $ _SERVER ['DB1_HOST'] '; port =' $ _SERVER ['DB1_PORT'] '; dbname =' $ _SERVER ['DB1_NAME'],
              'Username' => $ _SERVER ['DB1_USER'],
              'Password' => '',
          ),
