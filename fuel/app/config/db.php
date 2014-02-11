c95015ccaae54dc41373cd72e39e1bff7d8aab20
Exodus4341 <genz.ortiz@gmail.com>
2014-02-11 10:25:04 -0800

    db config

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * Use this file to override global defaults.
- *
- * See the individual environment DB configs for specific config information.
+ * The production database settings.
  */
 
 return array(
-     'Default' => array(
-         'Connection' => array(
-             'Dsn' =>'Mysql: host =' $_SERVER['DB1_HOST'] '; port =' $_SERVER['DB1_PORT'] '; dbname =' $_SERVER['DB1_NAME'],
-             'Username' => $_SERVER['DB1_USER'],
-             'Password' => '',
-         ),
-     ),
+    'default' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host='.$_SERVER['DB1_HOST'].';dbname='.$_SERVER['DB1_NAME'],
+            'username'   => $_SERVER['DB1_USER'],
+            'password'   => $_SERVER['DB1_PASS'],
+        ),
+    ),
 );
