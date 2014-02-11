9a6edee6683090f16a3a1be3a280152c2ed50dd9
Exodus4341 <genz.ortiz@gmail.com>
2014-02-11 09:39:45 -0800

    db config

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -5,6 +5,12 @@
  * See the individual environment DB configs for specific config information.
  */
 
-return array(
-
+return array (
+     'Default' => array (
+         'Connection' => array (
+             . 'Dsn' =>.... 'Mysql: host =' $ _SERVER ['DB1_HOST'] '; port =' $ _SERVER ['DB1_PORT'] '; dbname =' $ _SERVER ['DB1_NAME'],
+             'Username' => $ _SERVER ['DB1_USER'],
+             'Password' => '',
+         ),
+     ),
 );
